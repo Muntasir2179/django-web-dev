1f35c550ae66349082e75b602ed868b3b9d5121b
Muntasir2179 <muntasirahmed2179@gmail.com>
2024-11-24 20:43:06 +0600

    compacting each view function in a single view using dynamic segment of url feature

--- challenges/urls.py
+++ challenges/urls.py
@@ -2,7 +2,5 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('january', view=views.january, name="january"),
-    path('february', view=views.february, name="february"),
-    path('march', view=views.march, name="march"),
+    path('<month>', view=views.monthly_challenge, name="monthly_challenges"),
 ]
