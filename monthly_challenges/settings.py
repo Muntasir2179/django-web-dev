74aebaf443208b0085365d1a73147a8835a3a00a
Muntasir2179 <muntasirahmed2179@gmail.com>
2024-11-26 00:16:51 +0600

    added static files in app specific folders and root directory and configured necessary settings

--- monthly_challenges/settings.py
+++ monthly_challenges/settings.py
@@ -116,7 +116,13 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/5.0/howto/static-files/
 
-STATIC_URL = 'static/'
+STATIC_URL = 'static/'   # setting for deployment
+
+# django checks for static files in app specific directory
+# This setting tells django to also look for the static files in the static folder in root project directory
+STATICFILES_DIRS = [
+    BASE_DIR / "static"
+]
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.0/ref/settings/#default-auto-field
