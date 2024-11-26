021cd64856cad5b967fe39ee3cbd659ba72e1f0f
Muntasir2179 <muntasirahmed2179@gmail.com>
2024-11-26 11:33:35 +0600

    cleaning the main branch

--- challenges/urls.py
+++
@@ -1,8 +0,0 @@
-from django.urls import path
-from . import views
-
-urlpatterns = [
-    path('', view=views.index, name="index"),
-    path('<int:month>', view=views.monthly_challenge_by_number, name="monthly_challenge_by_number"),
-    path('<str:month>', view=views.monthly_challenge, name="monthly_challenges"),
-]
