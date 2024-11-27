996bbfc6d234988183ceee9a52912d33e4d4a5d5
Muntasir2179 <muntasirahmed2179@gmail.com>
2024-11-27 23:27:34 +0600

    added more fields to Book model with validators specifing null and default value

--- book_outlet/models.py
+++ book_outlet/models.py
@@ -1,7 +1,18 @@
 from django.db import models
+from django.core.validators import MinValueValidator, MaxValueValidator
 
 # Create your models here.
 
 class Book(models.Model):
     title = models.CharField(max_length=50)
-    rating = models.IntegerField()
+    rating = models.IntegerField(
+        validators=[
+            MinValueValidator(limit_value=1, message="Rating cannot be less then 1"),
+            MaxValueValidator(limit_value=5, message="Rating cannot be greater then 5")
+        ]
+    )
+    author = models.CharField(max_length=100, null=True)
+    is_bestselling = models.BooleanField(default=False)
+
+    def __str__(self):
+        return f"{self.title} ({self.rating})"
