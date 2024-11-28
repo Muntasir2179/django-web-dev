65e08286d5b8d70754da342cdc5a06c41548766e
Muntasir2179 <muntasirahmed2179@gmail.com>
2024-11-29 00:59:04 +0600

    performed aggregation with count(), aggregate() methods to get more information

--- book_outlet/views.py
+++ book_outlet/views.py
@@ -1,13 +1,25 @@
 from django.shortcuts import render, get_object_or_404
 from django.http import Http404
+from django.db.models import Avg, Max, Min
 
 from .models import Book
 
 # Create your views here.
 
 def index(request):
+    # if we want to order the data in some way
+    # books = Book.objects.all().order_by("rating")   # Ascending order
+    # books = Book.objects.all().order_by("-rating")   # Descending order
     books = Book.objects.all()
-    return render(request=request, template_name="book_outlet/index.html", context={"books": books})
+    num_books = books.count()
+    avg_rating = books.aggregate(Avg("rating"))
+    return render(request=request,
+                  template_name="book_outlet/index.html",
+                  context={
+                      "books": books,
+                      "total_number_of_books": num_books,
+                      "average_rating": avg_rating
+                  })
 
 
 def book_details(request, slug):
