0681379076644be3fc44d52af6f3d0b7197bba8d
Muntasir2179 <muntasirahmed2179@gmail.com>
2024-12-02 22:56:37 +0600

    FormView class used for handling get and post request automatically with form data saving into the database

--- reviews/views.py
+++ reviews/views.py
@@ -1,26 +1,20 @@
-from django.shortcuts import render
-from django.http import HttpResponseRedirect
-from django.views import View
 from django.views.generic.base import TemplateView
 from django.views.generic import ListView, DetailView
+from django.views.generic.edit import FormView
 
 from .forms import ReviewForm
 from .models import Review
 
 # Create your views here.
 
-class ReviewView(View):
-    def get(self, request):
-        form = ReviewForm()
-        return render(request=request, template_name="reviews/review.html", context={"form": form})
+class ReviewView(FormView):
+    form_class = ReviewForm   # modelform/form class to be used
+    template_name = "reviews/review.html"   # template to render
+    success_url = "/thank-you"  # url to redirect when successful form submission
 
-
-    def post(self, request):
-        form = ReviewForm(request.POST)  # validating the user entered form
-        if form.is_valid():
-            form.save()
-            return HttpResponseRedirect('/thank-you')
-        return render(request=request, template_name="reviews/review.html", context={"form": form})
+    def form_valid(self, form):   # what to do with form data
+        form.save()
+        return super().form_valid(form)
 
 
 class ThankYouView(TemplateView):
@@ -36,12 +30,6 @@ class ReviewListView(ListView):
     template_name = "reviews/reviews-list.html"   # template to render
     model = Review   # model class to use for fetching data
     context_object_name = "reviews"    # name of the context variable default name is "object_list"
-
-    # # suppose we want to fetch data conditionally
-    # def get_queryset(self):
-    #     base_query = super().get_queryset()
-    #     data = base_query.filter(rating__gt=3)
-    #     return data
 
 
 class SingleReviewView(DetailView):
