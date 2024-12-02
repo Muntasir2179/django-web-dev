e9d198ef560bc48e86e74297d0e984ab0a003ea6
Muntasir2179 <muntasirahmed2179@gmail.com>
2024-12-02 11:42:42 +0600

    added attributes to the form to show custom error message

--- reviews/views.py
+++ reviews/views.py
@@ -6,13 +6,15 @@ from .forms import ReviewForm
 
 def review(request):    
     if request.method == "POST":
-        form = ReviewForm(request.POST)
+        form = ReviewForm(request.POST)  # validating the user entered form
 
         if form.is_valid():
             print(form.cleaned_data)
             return HttpResponseRedirect('/thank-you')
+    else:
+        form = ReviewForm()
     
-    form = ReviewForm()
+    # if the form is not valid, django will attach some meaningful error messages with the form and if the request is GET then a fresh new form will be rendered
     return render(request=request, template_name="reviews/review.html", context={"form": form})
 
 
