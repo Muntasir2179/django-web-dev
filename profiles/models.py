02f223d50ebb5f1eebc79d3f3a6584c4142d56eb
Muntasir2179 <muntasirahmed2179@gmail.com>
2024-12-03 12:43:36 +0600

    using ImageField instead of FileField in form and model to be more specific about our data

--- profiles/models.py
+++ profiles/models.py
@@ -3,4 +3,4 @@ from django.db import models
 # Create your models here.
 
 class UserProfile(models.Model):
-    image = models.FileField(upload_to="images")   # name of the folder which will be used to store the file
+    image = models.ImageField(upload_to="images")   # name of the folder which will be used to store the file
