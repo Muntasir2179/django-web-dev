75ae8e213fa3dd5a1962ea0935cc0ae9f11a9376
Muntasir2179 <muntasirahmed2179@gmail.com>
2024-12-03 12:15:49 +0600

    using django form for creating file upload storing file manually using python files saving logic

--- profiles/views.py
+++ profiles/views.py
@@ -1,11 +1,27 @@
 from django.shortcuts import render
 from django.views import View
+from django.http import HttpResponseRedirect
+
+from .forms import ProfileForm
 
 # Create your views here.
 
+def store_file(file):
+    with open(f"temp/{file.name}", "wb+") as dest:
+        for chunk in file.chunks():
+            dest.write(chunk)
+
+
 class CreateProfileView(View):
     def get(self, request):
-        return render(request=request, template_name="profiles/create-profile.html")
+        form = ProfileForm()
+        return render(request=request, template_name="profiles/create-profile.html", context={"form": form})
     
     def post(self, request):
-        pass
+        submitted_form = ProfileForm(request.POST, request.FILES)
+
+        if submitted_form.is_valid():
+            print(request.FILES['image'].name)    # request has FILES attribute which contains all the uploaded files as a dictionary
+            store_file(request.FILES['image'])
+            return HttpResponseRedirect("/profiles")
+        return render(request=request, template_name="profiles/create-profile.html", context={"form": submitted_form})
