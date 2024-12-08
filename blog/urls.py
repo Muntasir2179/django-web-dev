ac387f040adec31aa33306950a035a00e29c0766
Muntasir2179 <muntasirahmed2179@gmail.com>
2024-12-08 15:50:27 +0600

    added read later feature for posts using session in a new class based view to render a new page containing the list of post selected for read later

--- blog/urls.py
+++ blog/urls.py
@@ -5,7 +5,8 @@ from . import views
 urlpatterns = [
     path("", view=views.StartingPageView.as_view(), name="starting-page"),
     path("post", view=views.PostsView.as_view(), name="posts-page"),
-    path("post/<slug:slug>", view=views.SinglePostView.as_view(), name="post-detail-page")
+    path("post/<slug:slug>", view=views.SinglePostView.as_view(), name="post-detail-page"),
+    path("read-later", view=views.ReadLaterView.as_view(), name="read-later"),
 ]
 
 
--- blog/views.py
+++ blog/views.py
@@ -29,13 +29,24 @@ class PostsView(ListView):
 
 
 class SinglePostView(View):
+    # function to find out whether currently rendering post is in the stored posts session or not
+    def is_stored_post(self, request, post_id):
+        stored_posts = request.session.get("stored_posts")
+        if stored_posts is not None:
+          is_saved_for_later = post_id in stored_posts
+        else:
+          is_saved_for_later = False
+
+        return is_saved_for_later
+    
     def get(self, request, slug):
         post = Post.objects.get(slug=slug)
         context = {
             "post": post,
             "post_tags": post.tags.all(),
             "comment_form": CommentForm(),
-            "comments": post.comments.all().order_by("-id")
+            "comments": post.comments.all().order_by("-id"),
+            "saved_for_later": self.is_stored_post(request, post.id)   # additional data to specify whether the post is stored for read later or not
         }
         return render(request=request, template_name="blog/post-detail.html", context=context)
     
@@ -53,7 +64,42 @@ class SinglePostView(View):
             "post": post,
             "post_tags": post.tags.all(),
             "comment_form": comment_form,
-            "comments": post.comments.all().order_by("-id")
+            "comments": post.comments.all().order_by("-id"),
+            "saved_for_later": self.is_stored_post(request, post.id)   # additional data to specify whether the post is stored for read later or not
         }
         return render(request=request, template_name="blog/post-detail.html", context=context)
 
+
+class ReadLaterView(View):
+    def get(self, request):
+        stored_posts = request.session.get("stored_posts")
+        context = {}
+
+        # building the context containing the read later posts fetched from session
+        if stored_posts is None or len(stored_posts) == 0:
+            context["posts"] = []
+            context["has_posts"] = False
+        else:
+            posts = Post.objects.filter(id__in=stored_posts)
+            context["posts"] = posts
+            context["has_posts"] = True
+        
+        return render(request=request, template_name="blog/stored-posts.html", context=context)
+
+    def post(self, request):
+        stored_posts = request.session.get("stored_posts")   # trying to access the stored_posts session variable
+
+        # if stored_posts variable not created in the session then create it
+        if stored_posts is None:
+            stored_posts = []
+        
+        post_id = int(request.POST["post_id"])   # fetching the post id from the request as it is sent via a hidden input filed
+
+        # append the post_id to the stored_posts
+        if post_id not in stored_posts:
+            stored_posts.append(post_id)
+        else:
+            stored_posts.remove(post_id)
+        
+        request.session["stored_posts"] = stored_posts   # keeping the selected post into the stored_posts session variable
+        return HttpResponseRedirect("/")   # redirecting to the current post detail page
